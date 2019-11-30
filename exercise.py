93b2ecedc1d50ed09b08ddcdbb0a64120393c33c
Chris <2019ks2020@gmail.com>
2019-11-30 21:29:37 +0100

    projekct begin

--- exercise.py
+++ exercise.py
@@ -1,8 +1,13 @@
-num1 = float(input("Podaj pierwsza liczbe:"))
-num2 = float(input("Podaj druga liczbe:"))
-
-wynik = num1 + num2
-print("Wynik to:", wynik)
+#Program napiwek
 
 
+wysokosc_rachunku = float(input("Podaj wysokość Twojego rachunku:"))
 
+napiwek15 = float(wysokosc_rachunku * 0.15)
+napiwek20 = float(wysokosc_rachunku * 0.20)
+
+
+
+print("Napiwek w wysokości 15 % wynosi:  ", napiwek15 , "\n")
+print("Napiwek w wysokości 20 % wynosi:  ", napiwek20 , "\n")
+input("Naciśnij klawisz Enter aby zakończyć program")
---
+++ jaka_to_liczba.py
@@ -0,0 +1,17 @@
+# Gra jaka to liczba polega na zgadywaniu w jak najmniejszej ilości prób liczb z zakresu od 1 do 1000.
+
+print("Witaj w grze 'Jaka to liczba'")
+
+import random
+
+losowa_liczba = random.ranint(1,1000)
+
+print("Spróbuj zgadnąć w jak najmniejszej liczbie prób liczbę z zakresu od 1 do 1000 ")
+
+liczba = int(input("Podaj szukaną liczbę, z zakresu od 1 do 1000: "))
+
+
+
+
+
+input("Aby zakończyć program, naciśnij klawisz Enter.")
